e82fad096343fecfb051f3825a2ea0a11c969bf4
tarequr <tarequrr8@gmail.com>
2024-07-12 22:04:42 +0600

    feat: add index.ejs template

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'EJS Template Engine',
+        message: 'Hello from EJS!',
+        members: ["Alamin", "Ruhul Amin", "Rana", "Tipu", "Sabbir", "Nayem", "Sharabon", "Rifat", "Raisan"],
+    });
 });
 
 app.listen(8000, () => console.log('Server started'));
